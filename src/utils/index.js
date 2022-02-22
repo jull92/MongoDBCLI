88ad1df7948ddd25c8c8dc378d77ff59cf500ae7
jull92 <julia.kata92@gmail.com>
2022-02-22 16:49:41 +0000

    first commit

---
+++ src/utils/index.js
@@ -0,0 +1,19 @@
+
+
+class Movie {
+    constructor (title, actor = "Not specified") {
+        this.title = title;
+        this.actor = actor;
+    }
+    async add (collection) {
+        // add this to the db
+        await collection.insertOne(this);
+        return "Success";
+    }
+    async list (collection) {
+        return await collection.find().toArray();
+        // list all movies in the db
+    }
+};
+
+module.exports = Movie;
