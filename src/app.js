d19cbd53c27f24d873a9170992ad90392a3b4fb8
Julia L <92579633+jull92@users.noreply.github.com>
2022-03-02 15:04:19 +0000

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -7,11 +7,19 @@ const app = async (yargsObj) => {
     try {
         if (yargsObj.add) {
             // take movie info, add it to the mongodb database and console.log a success msg
-            const movie = new Movie(yargsObj.title, yargsObj.actor);
+            const movie = new Movie(yargsObj.title, yargsObj.releaseYear, yargsObj.leadActor, yargsObj.genre);
             console.log(await movie.add(collection));
         } else if (yargsObj.list) {
             // list all movies in db
-            const movie = new Movie(yargsObj.title, yargsObj.actor);
+            const movie = new Movie(yargsObj.title, yargsObj.releaseYear, yargsObj.leadActor, yargsObj.genre);
+            console.log(await movie.list(collection));
+        // } else if (yargsObj.addMultiple) {
+        //     // add multiple 
+        //     const movie = new Movie(yargsObj.title, yargsObj.releaseYear, yargsObj.leadActor, yargsObj.genre);
+        //     console.log(await movie.list(collection));
+        } else if (yargsObj.update) {
+            // update db
+            const movie = new Movie(yargsObj.title, yargsObj.releaseYear, yargsObj.leadActor, yargsObj.genre);
             console.log(await movie.list(collection));
         } else {
             console.log("Incorrect command")
